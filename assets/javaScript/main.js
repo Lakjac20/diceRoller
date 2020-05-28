a349bd1a722a507f38f3ffd7cb1429a8bd0c7193
Lakwanjala <lakwanjalawilliams@gmail.com>
2020-05-27 19:35:47 -0500

    updated the sum of total rolls in HTML and JS

--- assets/javaScript/main.js
+++ assets/javaScript/main.js
@@ -2,9 +2,10 @@
 const dieRolls = []
 const rollButton = document.querySelector('#rollButton')
 let input = document.querySelector('input')
-//let totalRolled = document.querySelector('#total-rolled')//
 let allRolls = document.querySelector('#showAll')
 let listItems = document.querySelector('#rolls')
+const total = document.querySelector('#total')
+let all = 0
 
 
 
@@ -12,16 +13,17 @@ rollButton.addEventListener("click", function () {
     let diceInput = document.querySelector('#dice').value
 
     let counter = 0
-    let total = 0
+    
     while (counter < input.value) {
         diceInput = Math.floor(Math.random() * 6) + 1;
         counter += 1
-        total += diceInput
+        all += diceInput
         console.log(diceInput)
         dieRolls.push(diceInput);
+        
     }
 
-   
+   total.innerHTML = all
 })
 
 
